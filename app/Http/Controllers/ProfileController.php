2ab710c59009c0c0e78e47d4b29dd5e154a62372
s1kko1337 <r.melo.kk@gmail.com>
2024-05-08 20:05:13 +0300

    -

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
-use PhpParser\NodeVisitor\FirstFindingVisitor;
 
 class ProfileController extends Controller
 {
